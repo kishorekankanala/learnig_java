e5ad9e4588cc1333b71e882620b4f1038f26bac1
kanka@kishore <kankanalakishore9@gmail.com>
2023-05-10 14:13:23 -0400

    added hello world file

---
+++ src/main/java/com/java/learning/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.java.learning;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("welcome to hello world");
+        System.out.println("hi");
+    }
+}
